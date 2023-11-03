e47f5fd988f56a721403652e4e08ea5fb8eb8db0
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-03 13:55:19 +1000

    Add interactive buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header></Header>
@@ -15,6 +22,18 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("Component")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
