fafc9766bbc203288d0ae0bbc519a0647df8b95f
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-05 12:27:35 +1000

    Add prop forwarding

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,14 @@
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { useState } from "react";
-import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/examples.jsx";
 
 function App() {
-  const [tabContent, setTabContent] = useState();
-
-  function handleSelect(selectedButton) {
-    setTabContent(selectedButton);
-    console.log(selectedButton);
-  }
   return (
     <>
       <Header></Header>
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={tabContent === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Component
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={tabContent === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[tabContent].title}</h3>
-              <p>{EXAMPLES[tabContent].description}</p>
-              <pre>
-                <code>{EXAMPLES[tabContent].code}</code>
-              </pre>
-            </div>
-          ) : (
-            "Please select a topic"
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
---
+++ src/components/examples.jsx
@@ -0,0 +1,55 @@
+import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
+import { EXAMPLES } from "../data.js";
+import { useState } from "react";
+
+export default function Examples() {
+  const [tabContent, setTabContent] = useState();
+
+  function handleSelect(selectedButton) {
+    setTabContent(selectedButton);
+    console.log(selectedButton);
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <menu>
+        <TabButton
+          isSelected={tabContent === "components"}
+          onClick={() => handleSelect("components")}
+        >
+          Component
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "jsx"}
+          onClick={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "props"}
+          onClick={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={tabContent === "state"}
+          onClick={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent ? (
+        <div id="tab-content">
+          <h3>{EXAMPLES[tabContent].title}</h3>
+          <p>{EXAMPLES[tabContent].description}</p>
+          <pre>
+            <code>{EXAMPLES[tabContent].code}</code>
+          </pre>
+        </div>
+      ) : (
+        "Please select a topic"
+      )}
+    </Section>
+  );
+}
